42d84dabaa318198157ec0ba54150bb1e804a4a1
Андрей Миронов <amv_mail@mail.ru>
2022-09-17 20:10:38 +0300

    users_corrections

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.contrib.auth.views import (LoginView, LogoutView,
+                                       PasswordChangeDoneView,
+                                       PasswordChangeView,
+                                       PasswordResetCompleteView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetDoneView)
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import CreationForm
+
+
+class LoginView:
+    value = "{{csrf_token}}"
+    # success_url = reverse_lazy('posts:posts')
+    template_name = 'users/login.html'
+
+
+class LogoutView:
+    template_name = 'users/logged_out.html'
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    # После успешной регистрации перенаправляем пользователя на главную.
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/signup.html'
+
+
+class PasswordResetView:
+    success_url = reverse_lazy('users:password_reset_done')
+    template_name = 'users/password_reset_form.html'
+
+
+class PasswordResetDoneView:
+    # success_url = reverse_lazy('posts:posts')
+    template_name = 'users/password_reset_done.html'
+
+
+class PasswordResetConfirmView:
+    success_url = 'users/password_reset_done.html'
+    template_name = 'users/password_reset_confirm.html'
+
+
+class PasswordResetCompleteView:
+    template_name = 'users/password_reset_complete.html'
+
+
+class PasswordChangeView:
+    success_url = reverse_lazy('users/password_change_done.html')
+    template_name = 'users/password_change_form.html'
+
+
+class PasswordChangeDoneView:
+    template_name = 'users/password_change_done'
