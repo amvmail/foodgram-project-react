23007f15cb8b35f3f3f8a7046fd93d3a7e544920
Андрей Миронов <amv_mail@mail.ru>
2022-09-17 20:06:13 +0300

    core

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,7 +1,24 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls.static import static
+
+handler404 = 'core.views.page_not_found'
+handler403 = "core.views.permission_denied"
+handler500 = "core.views.server_error"
 
 urlpatterns = [
+    path('auth/', include('users.urls', namespace='users')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('', include('recipe.urls', namespace="recipe")),
     path('admin/', admin.site.urls),
+    path('about/', include('about.urls', namespace='about')),
 ]
+
+# if settings.DEBUG:
+#    import debug_toolbar
+#
+#    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+#    urlpatterns += static(
+#        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+#    )
