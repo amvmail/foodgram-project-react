a89fa7a5f5bfb26d60d78ca9cec93588150c5dc9
Андрей Миронов <amv_mail@mail.ru>
2022-10-12 22:38:55 +0300

    loaddata

--- backend/foodgram/recipes/management/commands/loaddata.py
+++ backend/foodgram/recipes/management/commands/loaddata.py
@@ -1,19 +1,21 @@
 import csv
+import os
 
 from django.core.management.base import BaseCommand
 
 from foodgram.settings import BASE_DIR
 from recipes.models import Ingredient
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-file_name = f'{BASE_DIR}/fixtures/ingredients.csv'
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+file_name = f'{BASE_DIR}/recipes/management/fixtures/ingredients.csv'
 
 
 class Command(BaseCommand):
+    help = 'Load ingredient data to database'
+
     def handle(self, *args, **options):
         with open(file_name) as csv_file:
             reader = csv.reader(csv_file)
             for line in reader:
-                # title, unit = line
                 title, dimension = line
-                Ingredient.objects.get_or_create(name=title, measurement_unit=dimension)
+                Ingredient.objects.get_or_create(title=title, dimension=dimension)
