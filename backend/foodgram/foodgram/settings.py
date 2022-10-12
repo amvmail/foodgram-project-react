9ef3dfa935b2e1bbb3d31195be68453dcf50ccdb
amvmail <amv_mail@mail.ru>
2022-10-12 23:25:01 +0300

    loadata

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -82,9 +82,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='foodgram'),
-        'USER': os.getenv('POSTGRES_USER'), #, default='postgres_foodgram'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='123456Aa'),
+        # 'NAME': os.getenv('DB_NAME', default='foodgram'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        # 'USER': os.getenv('POSTGRES_USER'), #, default='postgres_foodgram'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        # 'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='123456Aa'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         # 'HOST': os.getenv('DB_HOST', default='postgresql'),
         'HOST': os.getenv('DB_HOST', default='localhost'),
         'PORT': os.getenv('DB_PORT', default='5432')
