de0c90a4134189e905f6229b50ee7413cee0cbc2
Андрей Миронов <amv_mail@mail.ru>
2022-10-13 00:53:26 +0300

    load_data

---
+++ backend/foodgram/recipes/management/commands/load_data.py
@@ -0,0 +1,28 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    """
+    Команда 'load_ingredients' загружает ингредиенты
+    в базу из csv файла, который располагается в
+    директории /data/
+    """
+
+    def handle(self, *args, **options):
+        self.import_ingredients()
+        print('Загрузка ингредиентов завершена.')
+
+    def import_ingredients(self, file='recipes/management/fixtures/ingredients.csv'):
+        print(f'Загрузка {file}...')
+        file_path = f'{file}'
+        with open(file_path, newline='', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                status, created = Ingredient.objects.update_or_create(
+                    title=row[0],
+                    dimension=row[1]
+                )
