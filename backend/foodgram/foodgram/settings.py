975b8a06069436b70b5b232fecc0f2c9d18e3b16
Андрей Миронов <amv_mail@mail.ru>
2022-10-12 23:24:07 +0300

    loaddata

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='%_ivm!8z0jn9p1e!fcwh3mnsr_!h2dz9m54l)1vx9q%c+y)l#1')
 
-DEBUG = os.getenv('DEBUG', default=False)
+DEBUG = os.getenv('DEBUG', default=True)
 
 ALLOWED_HOSTS = ['*']
 
@@ -83,9 +83,9 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='foodgram'),
-        'USER': os.getenv('POSTGRES_USER'), #, default='postgres_foodgram'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres_foodgram'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='123456Aa'),
-        # 'HOST': os.getenv('DB_HOST', default='postgresql'),
+        'HOST': os.getenv('DB_HOST', default='postgresql'),
         'HOST': os.getenv('DB_HOST', default='localhost'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
