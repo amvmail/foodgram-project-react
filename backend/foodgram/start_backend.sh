732fc1da76535771456f41abc08c67fcf20c60b6
Андрей Миронов <amv_mail@mail.ru>
2022-10-13 20:46:11 +0300

    script

--- backend/foodgram/start_backend.sh
+++ backend/foodgram/start_backend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python3 manage.py migrate --noinput && \
 python3 manage.py collectstatic --noinput && \
+python3 manage.py load_data --noinput && \
 gunicorn foodgram.wsgi:application --bind 0:8000
