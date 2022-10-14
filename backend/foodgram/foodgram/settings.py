de4a0e2db8478a5f043769873eb11390aea71c08
amvmail <amv_mail@mail.ru>
2022-10-14 20:16:30 +0300

    some

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='%_ivm!8z0jn9p1e!fcwh3mnsr_!h2dz9m54l)1vx9q%c+y)l#1')
 
-DEBUG = os.getenv('DEBUG', default=True)
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['*']
 
@@ -82,11 +82,8 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        # 'NAME': os.getenv('DB_NAME', default='foodgram'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
-        #'USER': os.getenv('POSTGRES_USER', default='postgres_foodgram'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
-        # 'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='123456Aa'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='db'),
         # 'HOST': os.getenv('DB_HOST', default='localhost'),
--- backend/foodgram/start_backend.sh
+++ backend/foodgram/start_backend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# python3 manage.py makemigrations --noinput && \
 python3 manage.py migrate --noinput && \
 python3 manage.py collectstatic --noinput && \
 python3 manage.py load_data --noinput && \
