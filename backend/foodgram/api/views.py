e8a714244c3440ab7425fb558fcf2e9b988e9a5d
Андрей Миронов <amv_mail@mail.ru>
2022-10-16 20:00:00 +0300

    api_serializers

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -5,7 +5,7 @@ from recipes.models import (Amount, Favorite, Ingredient, Recipe, ShopList,
                             Subscription, Tag)
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from users.models import User
 
