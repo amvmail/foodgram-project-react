2a902f46116c3236c25e0921edeb13a26e209c5a
Андрей Миронов <amv_mail@mail.ru>
2022-10-22 18:07:37 +0300

    workflow

--- backend/foodgram/recipes/urls.py
+++ backend/foodgram/recipes/urls.py
@@ -1,6 +1,5 @@
-from api.views import (IngredientViewSet, RecipeViewSet, TagViewSet)
+from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
 from django.urls import include, path
-
 from rest_framework import routers
 
 router = routers.DefaultRouter()
