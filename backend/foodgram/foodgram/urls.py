d66c31a062fe5267a84774c9b3ffe443522c7f60
Андрей Миронов <amv_mail@mail.ru>
2022-10-22 23:37:52 +0300

    workflow

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,22 +1,7 @@
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Foodgram API",
-        default_version='',
-        description="Документация для приложения api проекта Foodgram",
-        contact=openapi.Contact(email="admin@foodgram.ru"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 
 urlpatterns = [
@@ -25,16 +10,6 @@ urlpatterns = [
     path('api/', include('recipes.urls')),
     path('api/', include('users.urls')),
 ]
-
-urlpatterns += [
-    url(r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'),
-]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
